d8ff93a95c805cceaaf14570d96f65aee4c7cb12
Lucas Quadros Batista <lucasgitt07@gmail.com>
2024-05-28 23:07:54 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Novo_Projeto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnMedia = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnMedia
+            // 
+            this.BtnMedia.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BtnMedia.Location = new System.Drawing.Point(247, 136);
+            this.BtnMedia.Name = "BtnMedia";
+            this.BtnMedia.Size = new System.Drawing.Size(102, 23);
+            this.BtnMedia.TabIndex = 0;
+            this.BtnMedia.Text = "VERIFICAR";
+            this.BtnMedia.UseVisualStyleBackColor = false;
+            this.BtnMedia.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // LblResult
+            // 
+            this.LblResult.AutoSize = true;
+            this.LblResult.Location = new System.Drawing.Point(318, 206);
+            this.LblResult.Name = "LblResult";
+            this.LblResult.Size = new System.Drawing.Size(31, 16);
+            this.LblResult.TabIndex = 3;
+            this.LblResult.Text = "........";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Produto esta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Insira a Quantidade";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(237, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nome";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(298, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(187, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Digite o nome do Produto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblResult);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.BtnMedia);
+            this.Name = "Form1";
+            this.Text = "Controle de estoque";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnMedia;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label LblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Novo_Projeto
+{
+   
+    public partial class Form1 : Form
+    {
+        public int valor=10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            valor = int.Parse(textBox1.Text);
+
+
+            if (valor <= 100)
+            {
+                LblResult.Text = "Faltando";
+            }
+            else if (valor <= 300)
+            {
+                LblResult.Text = "em estoque";
+
+            }
+            else if (valor >= 300)
+            {
+                LblResult.Text = "sobrando";
+
+            }
+
+            //switch (valor) { 
+
+            //    case 100:
+            //        LblResult.Text = "Faltando";
+            //        break;
+
+            //    case 300:
+            //        LblResult.Text = "em estoque";
+            //        break;
+
+            //    case 400:
+            //        LblResult.Text = "em estoque";
+            //        break;
+            //    default:
+            //        break;
+            // }
+
+            //Operador ternario
+
+            //    LblResult.Text = valor == 10 ? "SIM" : "NAO";
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
